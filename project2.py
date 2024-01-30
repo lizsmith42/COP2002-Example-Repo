20f99f19b2dd54fd39acf303ed1f5509f48bf98f
lizsmith42 <157316296+lizsmith42@users.noreply.github.com>
2024-01-29 17:21:58 -0800

    project2.py

--- project2.py
+++ project2.py
@@ -1,9 +1,9 @@
 print("My name is Elizabeth Smith. My major is Computer Science. I'm most interested in this class because I will be learning how to code in python which is useful in the field I want to go into.")
 x=22
 y=18
-print(x+y)
-print(x-y)
-print(x * y)
-print(x/y)
-print(x%y)
-print("This is the end of the program
+print("22+18="+str(x+y))
+print("22-18="+str(x-y))
+print("22*18="+str(x * y))
+print("22/18="+str(x/y))
+print("22%18="+str(x%y))
+print("This is the end of the program!")
